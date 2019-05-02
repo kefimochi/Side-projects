a4f5a9aefe8fc65627e7af235520ac7291639082
Kate Efimova <kate.efimova0@gmail.com>
2019-05-01 22:26:19 -0700

    Update colors.js

--- Femochi's Colors/colors.js
+++ Femochi's Colors/colors.js
@@ -18,101 +18,48 @@ function toggleNavigation(e) {
   }
 }
 
-<header id="header">
-  <i id="header-img" src="#"></i> <!--   For FCC -->
-  
-  <div class="nav-bar">
-    <a class="title" href="#first-section">Femochi's Colors</a>
-    <div class="toggle-bars">
-      <div class="bar"></div>
-      <div class="bar"></div>
-      <div class="bar"></div>
-    </div>
-    <div class="nav">
-      <ul>
-        <li class="nav-link"><a href="#first-section">Home</a></li>
-        <li class="nav-link"><a href="#second-section">About</a></li>
-        <li class="nav-link"><a href="#third-section">Subscription</a></li>
-        <li class="nav-link"><a href="#fourth-section">Order</a></li>
-        <li class="nav-link"><a href="#fifth-section">Contact</a></li>
-      </ul>
-    </div>
-  </div>
-</header>
-  
-  <div class="media-links">
-    <ul class="media-links-content">
-      <li>
-        <i class="fab fa-facebook-f"></i>
-        <span>Facebook</span>
-      </li>
-      <li class="twitter">
-        <i class="fab fa-twitter"></i>
-        <span>Twitter</span>
-      </li>
-        <li class="google">
-        <i class="fab fa-google-plus-g"></i>
-        <span>Google +</span>
-      </li>
-    </ul>
-  </div>
-  
-  <section id="first-section">
-    <div class="transparent"></div>
-    <div class="transparent-2"></div>
-    <div class="transparent-3"></div>
-    <div class="main-tr">
-    </div>
-    <h1 class="sec1-title">Femochi's Colors</h1>
-    <h1 class="sec1-title-shadow">Femochi's Colors</h1>
-    <div class="cursor-text-container">
-      <p>Watercoloring is <span class="typed-text"></span><span class="cursor">&nbsp;</span></p>
-    </div>
-  </section>
-  
-  <section id="second-section">
-    <div class="icon-space">    
-      <div class="icons">
-        <div class="icon">
-          <i class="fas fa-palette"></i>  
-        </div>
-        <h1 class="icon-title">Professional Quality</h1>
-        <h1 class="icon-text">All colors are created using professional single pigments. We also use gum arabic and honey as a watercolor binder in production.</h1>
-      </div>
-      
-      <div class="icons">
-        <div class="icon">
-          <i class="fas fa-paint-brush"></i>  
-        </div>
-          <h1 class="icon-title">Fast Shipping</h1>
-          <h1 class="icon-text">A guranteed delievery of your product in 3 days after you place an order. yes, it applies worldwide.</h1>
-      </div>
-      
-      <div class="icons">
-        <div class="icon">
-           <i class="fas fa-truck"></i>
-        </div>
-           <h1 class="icon-title">Wide selection</h1>
-           <h1 class="icon-text">More than 364 colors are available for purchase already with new colors released every month.</h1>
-      </div>
-    </div>
-    <div class="about-container">
-      <div class="about-text-container">
-        <h1 class="about-title">ABOUT</h1>
-        <h1>Since around 1970s our team at Femochi's Colors were delievering bright proffesional quality watercolors right next to your door. Starting 2019 there is now an option of subscribing monthly, where automatically(w/o worries)clients will be charged a certain amount of money every month for a monthly shipping. <br/> It is cheaper compared to buying watercolors pans that are in stock, so if you are a constant watercolor user and professional/hobbiest make sure to consider this cheaper option! Please don't take this text seriously, its just here to make this section look pretty.</h1>
-      </div>
-    <img class="about-image" src="https://github.com/Kefimochi/Random-stuff/blob/master/Femochi's%20Colors/Done.png?raw=true" alt="Watercolor set" height="420" width="540">
-      </div>
-  </section>
-  
-  <section id="third-section">
-  </section>
-  
-  <section id="fourth-section">
-  </section>
-  
-  <section id="fifth-section">
-  </section>
-  
- <footer>
-  </footer>
+
+
+
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["hard", "fun", "a journey", "LIFE"];
+const typingSpeed = 200;
+const erasingSpeed = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0; // textArray index
+let charIndex = 0; // character index
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingSpeed);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingSpeed);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingSpeed + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  setTimeout(type, newTextDelay + 250);
+});
