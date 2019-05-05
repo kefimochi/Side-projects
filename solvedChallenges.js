72bf9f09f0efd1480952cc623e8fd65ea6310d08
Kate Efimova <kate.efimova0@gmail.com>
2019-05-04 20:34:02 -0700

    Update solvedChallenges.js

--- solvedChallenges.js
+++ solvedChallenges.js
@@ -12,3 +12,21 @@ function main() {
     let result = sockMerchant(7, [0,2,10,2,3,4,4]);
     console.log(result + "\n")
 }
+/**************************************************Match String Challenge**********************************************/
+let wordDict = ["facebook", "face", "boo", "k", "haha", "book"]
+let s = 'facebook';
+
+function joinString(s, wordDict) {
+  let arr = [];
+  let str = ''; //Push it into arr, then reuse
+  // if wordDict[i] = s, then add it to a new array
+  for (let i = 0; i < wordDict.length; i++) {
+    if (wordDict[i] === s) arr.push(wordDict[i]);
+}
+  
+  console.log(s);
+  console.log(wordDict);
+  return arr;
+}
+
+console.log(joinString(s, wordDict));
