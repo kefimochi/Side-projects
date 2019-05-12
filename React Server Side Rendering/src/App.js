4897dece24a1ea1b3b79a2e4240c6e2775b7b93d
Kate Efimova <kate.efimova0@gmail.com>
2019-05-12 09:17:11 -0700

    Add files via upload

---
+++ React Server Side Rendering/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Stateless functional component
+// Returns JSX
+function App(props) {
+  const facts = props.facts.map((fact, i) => {
+    return <li key={i}>{fact.next}</li>;
+  });
+  return <ul>{facts}</ul>;
+}
+
+export default App;
