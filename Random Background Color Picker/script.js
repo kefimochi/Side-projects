d9640793b8183fb5e259045fc80f720b177d78c2
Kate Efimova <kate.efimova0@gmail.com>
2019-06-06 12:42:50 -0700

    Added hex colors

--- Random Background Color Picker/script.js
+++ Random Background Color Picker/script.js
@@ -1,43 +1,37 @@
 function randomColor() {
-  let colors = [
-    "blue",
-    "red",
-    "yellow",
-    "orange",
-    "green",
-    "rebeccapurple",
-    "black"
-  ];
-  shuffle(colors);
-  if (colors[0] !== "yellow") {
-    document.documentElement.style.setProperty(`--random-color`, colors[0]);
-    refactorText("Current color used: " + colors[0]);
-    document.documentElement.style.setProperty(`--primary`, 'white');
+  let color = getRandomColor();
+  if (color !== "yellow") {
+    //some function checking for accessibility
+    document.documentElement.style.setProperty(`--primary`, "white");
   } else {
-    document.documentElement.style.setProperty(`--random-color`, colors[0]);
-    refactorText("Current color was not accessible, defaulted to black!");
-    document.documentElement.style.setProperty(`--primary`, 'black');
+    document.documentElement.style.setProperty(`--primary`, "black");
   }
+  refactorText(color);
+  document.documentElement.style.setProperty(`--random-color`, color);
+}
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 function refactorText(text) {
-  let element = document.querySelector(".sub-text");
+  let element = document.querySelector(".random-color");
   element.textContent = text;
 }
 
-let shuffle = array => {
-  let currentIndex = array.length;
-  let temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+// let shuffle = array => {
+//   let currentIndex = array.length;
+//   let temporaryValue, randomIndex;
+//   while (0 !== currentIndex) {
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex -= 1;
 
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-};
+//     temporaryValue = array[currentIndex];
+//     array[currentIndex] = array[randomIndex];
+//     array[randomIndex] = temporaryValue;
+//   }
+//   return array;
+// };
 
 let button = document.querySelector(".btn");
 button.addEventListener("click", randomColor);
