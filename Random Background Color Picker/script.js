2e7443eb1c68f7820142651c09dc275469101517
Kate Efimova <kate.efimova0@gmail.com>
2019-06-07 23:36:47 -0700

    Made 'copy to clipboard' feature work, woohoo!

--- Random Background Color Picker/script.js
+++ Random Background Color Picker/script.js
@@ -34,3 +34,12 @@ function copyColor() {
 
 let button = document.querySelector(".btn");
 button.addEventListener("click", randomColor);
+
+function CopyToClipboard(containerid) {
+  var range = document.createRange();
+  range.selectNode(document.getElementById(containerid));
+  window.getSelection().removeAllRanges(); // clear current selection
+  window.getSelection().addRange(range); // to select text
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges(); // to deselect
+}
