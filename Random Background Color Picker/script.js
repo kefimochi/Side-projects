5406cc2fe9eba9becae5990a8100cb162a6a8845
Kate Efimova <kate.efimova0@gmail.com>
2019-06-08 20:34:09 -0700

    Fixed a small bug + refactored

--- Random Background Color Picker/script.js
+++ Random Background Color Picker/script.js
@@ -1,5 +1,5 @@
-let generatedColor = "#2d1dba";
-let generatedColor2 = "#f772ff";
+let generatedColor;
+let generatedColor2;
 
 function randomColor() {
   generatedColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -22,32 +22,20 @@ function refactorText(text, text2) {
   let element = document.querySelector("#random-color");
   element.textContent = text;
   let element2 = document.querySelector("#random-color-2");
-  element.textContent = text2;
+  element2.textContent = text2;
 }
 
-function CopyToClipboard(containerid) {
-  var range = document.createRange();
-  range.selectNode(document.getElementById(containerid));
-  window.getSelection().removeAllRanges(); // clears current selection
-  window.getSelection().addRange(range); // to select text
-  document.execCommand("copy");
-  window.getSelection().removeAllRanges(); // to deselect text
-  alert("Color was successfully copied!");
-}
-
-function copy() {
+function copyColors() {
   var el = document.createElement("textarea");
   // Set value (string to be copied)
   el.value = `linear-gradient(to top right, ${generatedColor},
     ${generatedColor2});`;
+
   // Set non-editable to avoid focus and move outside of view
   el.setAttribute("readonly", "");
   el.style = { position: "absolute", left: "-9999px" };
   document.body.appendChild(el);
-  // Select text inside element
   el.select();
-  // Copy text to clipboard
   document.execCommand("copy");
-  // Remove temporary element
   document.body.removeChild(el);
 }
