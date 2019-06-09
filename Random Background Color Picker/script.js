cbdd96388084c861d0521aca704171e609bdf1db
Kate Efimova <kate.efimova0@gmail.com>
2019-06-08 21:49:47 -0700

    Added switching between white & black text

--- Random Background Color Picker/script.js
+++ Random Background Color Picker/script.js
@@ -2,15 +2,22 @@ let generatedColor = "#2d1dba";
 let generatedColor2 = "#f772ff";
 
 function randomColor() {
-  generatedColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  generatedColor2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  generatedColor = Math.floor(Math.random() * 16777215).toString(16);
+  generatedColor2 = Math.floor(Math.random() * 16777215).toString(16);
 
-  if (generatedColor !== "#1f37d") {
+  if (
+    getContrastYIQ(generatedColor) === "black" &&
+    getContrastYIQ(generatedColor2) === "black"
+  ) {
     //some function checking for accessibility
-    document.documentElement.style.setProperty(`--primary`, "white");
-  } else {
     document.documentElement.style.setProperty(`--primary`, "black");
+  } else {
+    document.documentElement.style.setProperty(`--primary`, "white");
   }
+
+  generatedColor = "#" + generatedColor;
+  generatedColor2 = "#" + generatedColor2;
+
   refactorText(generatedColor, generatedColor2);
 
   document.documentElement.style.setProperty(`--random-color`, generatedColor);
@@ -41,4 +48,12 @@ function copyColors() {
   document.execCommand("copy");
   document.body.removeChild(el);
   alert("Color was successfully copied!");
+}
+
+function getContrastYIQ(hexcolor) {
+  var r = parseInt(hexcolor.substr(0, 2), 16);
+  var g = parseInt(hexcolor.substr(2, 2), 16);
+  var b = parseInt(hexcolor.substr(4, 2), 16);
+  var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? "black" : "white";
 }
