bda41705f31245af985403c30f4f86065b12213d
Kate Efimova <kate.efimova0@gmail.com>
2019-06-25 23:29:14 -0700

    Added generating a number 0-4 for array of imgs

--- Division of Pictures/script.js
+++ Division of Pictures/script.js
@@ -1,4 +1,4 @@
-let links = [
+let images = [
   "https://planto-the-plant-watering-app.herokuapp.com/assets/plants/plant1.png",
   "https://planto-the-plant-watering-app.herokuapp.com/assets/plants/plant2.png",
   "https://planto-the-plant-watering-app.herokuapp.com/assets/plants/plant3.png",
@@ -12,4 +12,17 @@ plants.forEach(plant => {
 
 function multiply() {
   console.log("Hovered");
+  let img = document.createElement("img");
+  img.src = images[generateNum()];
+  document.getElementById("cont1").appendChild(img);
+  // Somehow checks which container is the parent.
+  // In ideal world, the element would be creates right next to
+  // the hovered one, yet a bit offsetted to the left or right(might be determined
+  // on which container).
+}
+
+function generateNum() {
+  let min = Math.ceil(0);
+  let max = Math.floor(4);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
