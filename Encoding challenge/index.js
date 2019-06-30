953f5e28fdbfd4058745d68456f0c84e7045fbfc
Kate Efimova <kate.efimova0@gmail.com>
2019-06-29 20:56:22 -0700

    Converts to binary

--- Encoding challenge/index.js
+++ Encoding challenge/index.js
@@ -1,8 +1,6 @@
 function encode(num) {
   let encodedNum = num + 8192;
   encodedNum = encodedNum.toString(2);
-
-  let el = 11;
   // Interprets given num as binary, thus 11 as input will result in 3
   //   console.log("Testing binary: ", Number.parseInt(11, 2));
   //   console.log("Testing binary 2: ", el.toString(2));
@@ -13,6 +11,10 @@ function decode(num) {
   return num;
 }
 
-console.log("Encoding 0 (should be 4000):", encode(0));
+String.prototype.splice = function(idx, rem) {
+  return this.slice(0, idx) + 0 + this.slice(idx + Math.abs(rem));
+};
+
+console.log("Encoding 0 (should be 4000):", encode(11));
 console.log("Encoding -8192 (should be 0000):", encode(-8192));
 console.log("Encoding 2048 (should be 5000):", encode(2048));
