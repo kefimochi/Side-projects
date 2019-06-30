4f31acf16c5c6889ff7f93f7e5274eb2fd17a4c1
Kate Efimova <kate.efimova0@gmail.com>
2019-06-30 10:11:04 -0700

    Added hexToBin conversion

--- Encoding challenge/index.js
+++ Encoding challenge/index.js
@@ -10,8 +10,48 @@ function encode(num) {
 
 function decode(byte1, byte2) {
   let joined = byte1 + byte2;
-  console.log(joined);
+  //   console.log(joined);
 }
+
+function hexToBin(src) {
+  // Kills any 0's at the start of the string since
+  // they will not play any role in conversion
+  while (src.charAt(0) === "0") {
+    src = src.substr(1);
+  }
+
+  let mapping = {
+    "0": "0000",
+    "1": "0001",
+    "2": "0010",
+    "3": "0011",
+    "4": "0100",
+    "5": "0101",
+    "6": "0110",
+    "7": "0111",
+    "8": "1000",
+    "9": "1001",
+    A: "1010",
+    B: "1011",
+    C: "1100",
+    D: "1101",
+    E: "1110",
+    F: "1111"
+  };
+
+  //If starts with zero - delete
+
+  src = src.toString();
+  var returnString = "";
+
+  for (let i = 0; i < src.length; i++) {
+    returnString += mapping[src[i]];
+  }
+
+  return returnString;
+}
+
+console.log("Hex to bin", hexToBin("A05"));
 
 String.prototype.splice = function(idx, rem) {
   return this.slice(0, idx) + 0 + this.slice(idx + Math.abs(rem));
