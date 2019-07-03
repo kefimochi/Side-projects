7c831d2712992815fc7c70dc49a5bff36fb91a7b
Kate Efimova <kate.efimova0@gmail.com>
2019-07-03 14:55:04 -0700

    Added basic server js

--- Email Form/script.js
+++ Email Form/script.js
@@ -1,3 +1,36 @@
-function myFunction() {
-    alert("Form successfully recieved!");
-}
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.post("/", (req, res) => {
+  res.send(
+    "Your name is " + req.body.name + " and your email is " + req.body.email
+  );
+
+  fs.appendFile(
+    "database.txt",
+    "name: " + req.body.name,
+    "email" + req.body.email
+  ),
+    err => {
+      if (err) throw Error;
+    };
+});
+
+let options = {
+  index: "index.html"
+};
+
+app.use(express.static("public", options));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}.`));
