482780ba0e526f01a44f37123ca4a492ae903d81
Kate Efimova <kate.efimova0@gmail.com>
2019-07-04 12:11:20 -0700

    Adding the picture to public folder for visibility

--- Email Form/script.js
+++ Email Form/script.js
@@ -18,21 +18,22 @@ app.post("/", (req, res) => {
     "Your name is " + req.body.name + " and your email is " + req.body.email
   );
 
-  fs.appendFile("database.txt", "name: " + req.body.name + "\n", err => {
-    if (err) throw Error;
-  });
-  fs.appendFile("database.txt", "email: " + req.body.email + "\n", err => {
-    if (err) throw Error;
-  });
-  fs.appendFile("database.txt", "message: " + req.body.message + "\n", err => {
-    if (err) throw Error;
-  });
-  fs.appendFile("database.txt", "****************\n", err => {
-    if (err) throw Error;
-  });
+  //   fs.appendFile("database.txt", "name: " + req.body.name + "\n", err => {
+  //     if (err) throw Error;
+  //   });
+  //   fs.appendFile("database.txt", "email: " + req.body.email + "\n", err => {
+  //     if (err) throw Error;
+  //   });
+  //   fs.appendFile("database.txt", "message: " + req.body.message + "\n", err => {
+  //     if (err) throw Error;
+  //   });
+  //   fs.appendFile("database.txt", "****************\n", err => {
+  //     if (err) throw Error;
+  //   });
 
   MongoClient.connect("mongodb://localhost:27017/", function(err, db) {
-    let dbo = db.db("contacts").insertOne(
+    var dbo = db.db("contacts");
+    dbo.collection("contacts").insertOne(
       {
         name: req.body.name,
         email: req.body.email,
